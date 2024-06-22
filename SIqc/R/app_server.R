ea6169cea7a58a89de5318e299b60926dda36255
andreabz <andrea.bazzano86@gmail.com>
2024-06-22 16:26:49 +0200

    solved the bug of stacked modal

--- SIqc/R/app_server.R
+++ SIqc/R/app_server.R
@@ -7,35 +7,40 @@
 #' @importFrom RSQLite SQLite
 #' @noRd
 app_server <- function(input, output, session) {
-
   conn <- pool::dbPool(
     drv = RSQLite::SQLite(),
     dbname = "./data/sqlite_test.db",
     extended_types = TRUE
-    )
+  )
   onStop(function() {
     pool::poolClose(conn)
   })
 
-  r_global <- reactiveValues(conn = conn,
-                             taskid = NA,
-                             activity = NA,
-                             completed = NA,
-                             edit_results = 0,
-                             dbtrigger = NA)
+  r_global <- reactiveValues(
+    conn = conn,
+    taskid = NA,
+    activity = NA,
+    completed = NA,
+    edit_results = 0,
+    dbtrigger = NA
+  )
 
   mod_01_plan_server("tasks", r_global)
 
-  observeEvent(r_global$edit_results, {
-    req(!is.na(r_global$activity))
+  observeEvent(r_global$edit_results,
+               {
+                 req(!is.na(r_global$activity))
 
-    switch (
-      r_global$activity,
-      "ripetibilità" = mod_011_repeatability_server("rep", r_global),
-      "giustezza" = NA,
-      "proficency test" = NA
-    )
+                 switch (
+                   r_global$activity,
+                   "ripetibilità" = mod_011_repeatability_server("rep", r_global),
+                   "giustezza" = NA,
+                   "proficency test" = NA
+                 )
 
-  })
+               },
+               # very important to avoid the bug of stacked modal dialog
+               once = TRUE,
+               ignoreInit = TRUE)
 
 }
--- SIqc/R/mod_011_repeatability.R
+++ SIqc/R/mod_011_repeatability.R
@@ -7,15 +7,19 @@ mod_011_repeatability_server <- function(id, r_global) {
     conn <- isolate(r_global$conn)
     dbtrigger <- makereactivetrigger()
 
-    r_local <- reactiveValues(sample_results = data.table::data.table(),
-                              sample_ids = NA,
-                              sample_name1 = NA,
-                              sample_name2 = NA,
-                              task_result = NA,
-                              task_comment = NA)
+    r_local <- reactiveValues(
+      sample_results = data.table::data.table(),
+      sample_ids = NA,
+      sample_name1 = NA,
+      sample_name2 = NA,
+      task_result = NA,
+      task_comment = NA
+    )
 
-    shiny::observeEvent(r_global$taskid, {
+    shiny::observeEvent(r_global$edit_results, {
+      req(r_global$activity == "ripetibilità")
 
+      # get and stores input values
       r_local$sample_ids <- sql_get_sampleid_for_task(conn, r_global$taskid)
       r_local$sample_name1 <- sql_get_name(conn, "campione", r_local$sample_ids[1])
       r_local$sample_name2 <- sql_get_name(conn, "campione", r_local$sample_ids[2])
@@ -23,94 +27,86 @@ mod_011_repeatability_server <- function(id, r_global) {
       r_local$task_comment <- sql_get_comment_for_task(conn, taskid = r_global$taskid)
 
       # update the inputs only when sample information has been saved
-      if(length(r_local$sample_ids) == 2){
+      if (length(r_local$sample_ids) == 2) {
         freezeReactiveValue(input, "sample1")
         updateSelectInput(session, "sample1", selected = r_local$sample_name1)
         freezeReactiveValue(input, "sample2")
         updateSelectInput(session, "sample2", selected = r_local$sample_name2)
       }
 
+      # update result and comment
       freezeReactiveValue(input, "result")
       updateSelectInput(session, "result", selected = r_local$task_result)
       freezeReactiveValue(input, "comment")
       updateSelectInput(session, "comment", selected = r_local$task_comment)
 
-    })
-
-    shiny::observeEvent(r_global$edit_results, {
-      req(r_global$activity == "ripetibilità")
-
+      # load the modal dialog
       repeatability_modal(conn = conn, id, completed = r_global$completed)
       shinyjs::hide("save_res")
     })
 
+    # update data results when samples are changed
     shiny::observeEvent(c(input$sample1, input$sample2), {
-
-      r_local$sample_results <- sql_get_repeatability(conn,
-                                                      input$sample1,
-                                                      input$sample2,
-                                                      mytask = r_global$taskid)
+      r_local$sample_results <- sql_get_repeatability(conn, input$sample1, input$sample2, mytask = r_global$taskid)
     })
 
-    result_tbl <- reactive({repeatabilityDT(r_local$sample_results)})
+    # table of data results
+    result_tbl <- reactive({
+      repeatabilityDT(r_local$sample_results)
+    })
     output$dt_data <- DT::renderDT(result_tbl())
 
-      # when the table is edited compliance is assessed
-      observeEvent(input$dt_data_cell_edit, {
-        r_local$sample_results <- DT::editData(
-          r_local$sample_results,
-          input$dt_data_cell_edit,
-          "dt_data",
-          rownames = FALSE
-        )
-        r_local$sample_results$esito <- ifelse(
-          r_local$sample_results$differenza <= r_local$sample_results$requisito,
-          "conforme",
-          "non conforme"
-        )
+    # when the table is edited compliance is assessed
+    observeEvent(input$dt_data_cell_edit, {
+      r_local$sample_results <- DT::editData(r_local$sample_results,
+                                             input$dt_data_cell_edit,
+                                             "dt_data",
+                                             rownames = FALSE)
+      r_local$sample_results$esito <- ifelse(
+        r_local$sample_results$differenza <= r_local$sample_results$requisito,
+        "conforme",
+        "non conforme"
+      )
 
-      })
+    })
 
-      ###### close modal ----
-      shiny::observeEvent(input$close_btn, {
-        shiny::removeModal()
-      })
+    ###### close modal ----
+    shiny::observeEvent(input$close_btn, {
+      shiny::removeModal()
+    })
 
-      # enable save button only when limit and final result are reported
-      observeEvent(c(input$dt_data_cell_edit,
-                     input$result,
-                     input$comment), {
+    # enable save button only when limit and final result are reported
+    observeEvent(c(input$dt_data_cell_edit, input$result, input$comment),
+                 {
+                   req_completed <- sum(!is.na(as.numeric(r_local$sample_results$requisito)))
+                   n_parameter <- r_local$sample_results$parametro |> length()
 
-        req_completed <- sum(!is.na(as.numeric(r_local$sample_results$requisito)))
-        n_parameter <- r_local$sample_results$parametro |> length()
+                   if (req_completed == n_parameter && !is.null(input$result)) {
+                     shinyjs::show("save_res")
+                   }
 
-        if(req_completed == n_parameter && !is.null(input$result)){
-          shinyjs::show("save_res")
-        }
+                 })
 
-      })
+    ###### save modal ----
+    shiny::observeEvent(input$save_res, {
+      sql_mod_repeatability(
+        conn,
+        sample1 = input$sample1,
+        sample2 = input$sample2,
+        mytask = r_global$taskid,
+        mydata = r_local$sample_results
+      )
 
-      ###### save modal ----
-      shiny::observeEvent(input$save_res, {
+      sql_mod_result(
+        conn,
+        result = input$result,
+        comment = input$comment,
+        mytask = r_global$taskid
+      )
 
-        sql_mod_repeatability(
-          conn,
-          sample1 = input$sample1,
-          sample2 = input$sample2,
-          mytask = r_global$taskid,
-          mydata = r_local$sample_results
-        )
-
-        sql_mod_result(
-          conn,
-          result = input$result,
-          comment = input$comment,
-          mytask = r_global$taskid
-        )
-
-        r_global$dbtrigger <- dbtrigger$trigger()
-        shiny::removeModal()
-      })
+      r_global$dbtrigger <- dbtrigger$trigger()
+      shiny::removeModal()
+    })
 
   })
 }
